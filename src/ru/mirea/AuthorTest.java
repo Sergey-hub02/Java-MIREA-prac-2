a24450cb6311087814d9c788c3953c02b02560b1
Sergey-hub02 <s.park190802@gmail.com>
2021-09-11 09:46:51 +0300

    Первое задание выполнено, приступаю ко второму

--- src/ru/mirea/AuthorTest.java
+++ src/ru/mirea/AuthorTest.java
@@ -1,12 +1,26 @@
 package ru.mirea;
 
 import java.util.Scanner;
+import ru.mirea.classes.Author;
 
 
 public class AuthorTest {
   private static final Scanner IN = new Scanner(System.in);
 
   public static void main(String[] args) {
-    ;
+    System.out.print("Введите имя автора: ");
+    String name = IN.nextLine();
+
+    System.out.print("Введите email автора: ");
+    String email = IN.next();
+
+    System.out.print("Введите пол автора: ");
+    char gender = IN.next().toCharArray()[0];
+
+    Author author = new Author(name, email, gender);
+
+    System.out.println();
+    System.out.println("Информация об авторе:");
+    System.out.println(author);
   }
 }
--- src/ru/mirea/classes/Author.java
+++ src/ru/mirea/classes/Author.java
@@ -16,4 +16,44 @@ public class Author {
     this.email = email;
     this.gender = gender;
   }
+
+  /**
+   * Геттер для поля name
+   * @return        имя автора
+   */
+  public String getName() {
+    return this.name;
+  }
+
+  /**
+   * Геттер для поля email
+   * @return        email автора
+   */
+  public String getEmail() {
+    return this.email;
+  }
+
+  /**
+   * Сеттер для поля email
+   * @param email       новый email автора
+   */
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  /**
+   * Геттер для поля gender
+   * @return      пол автора
+   */
+  public char getGender() {
+    return this.gender;
+  }
+
+  /**
+   * Объединяет информацию об объекте в одну строку
+   * @return        строка с информацией об объекте
+   */
+  public String toString() {
+    return this.name + " (" + this.gender + ") at " + this.email;
+  }
 }
